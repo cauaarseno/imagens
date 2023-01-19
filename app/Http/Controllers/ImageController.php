0664b995995edb816dfe87069ce4768d764ce09c
Cauã Arseno <cauaarseno61@gmail.com>
2023-01-19 00:49:02 -0300

    first commit

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImageController extends Controller
+{
+    public function image(Request $request)
+    {
+        $request->image1->storeAs('public/images','file1.jpg');
+        $request->image2->storeAs('public/images','file2.jpg');
+    }
+}
